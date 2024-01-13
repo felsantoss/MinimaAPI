9a4eee85fc17aaf32f6d67a1e17454ee197dd57d
felsantoss <felipesantossf@outlook.com>
2024-01-13 11:13:42 -0300

    Criação da classe Modelo, o modelo posteriormente o EF Core mapeara esse modelo de dados com uma tabela no banco de dados

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-     c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
+     c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", 
+                                          Description = "Making the Pizzas you love", 
+                                          Version = "v1" });
 });
     
 var app = builder.Build();
