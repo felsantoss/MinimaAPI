7cf0c82adc78a6d1004b72663d51b3621c13bb16
Felipe Santos <felipesantossf@outlook.com>
2024-02-07 21:15:18 -0300

    Restore all files

--- obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be44bf1c9520084ee6ef632df6a64e2928a6c42a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b48debe0c46ede8864f0d825ecc6b27831635bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
